ae6cd100da6ad96e750f70b957607b532d689f4e
Joseph Jager <j0j01tx@m-c02xd0h8jgh7.homeoffice.wal-mart.com>
2019-04-12 10:50:39 -0700

    moved memcached client

--- api/app.js
+++ api/app.js
@@ -9,12 +9,36 @@ Bailouts should ensure the connection is not reused/verified until at least 5m (
 Validated in both stg0 and stg1
 */
 
-const port              = 3000
-const multiServerOptions = {
+const localServerOptions = {
   server: {
     servers: [
       {
         server: "localhost:8000",
+        maxConnections: 3
+      }
+    ],
+    config: {
+      cmdTimeout: 5 * 1000,
+      retryFailedServerInterval: 1000,    // milliseconds - how often to check failed servers
+      failedServerOutTime: 5 * 60 * 1000, // (ms) how long a failed server should be out before retrying it
+      keepLastServer: true
+    }
+  }
+}
+
+const multiServerOptions = {
+  server: {
+    servers: [
+      {
+        server: "localhost:5000",
+        maxConnections: 3
+      },
+      {
+        server: "localhost:5001",
+        maxConnections: 3
+      },
+      {
+        server: "localhost:5002",
         maxConnections: 3
       }
     ],
@@ -26,46 +50,9 @@ const multiServerOptions = {
     }
   }
 }
-/*
-const multiServerOptions = {
-    server: {
-      servers: [
-        {
-          server: "localhost:5000",
-          maxConnections: 3
-        },
-        {
-          server: "localhost:5001",
-          maxConnections: 3
-        },
-        {
-          server: "localhost:5002",
-          maxConnections: 3
-        }
-      ],
-      config: {
-        cmdTimeout: 5 * 1000,
-        retryFailedServerInterval: 1000, // milliseconds - how often to check failed servers
-        failedServerOutTime: 1000, //5 * 60 * 1000, // (ms) how long a failed server should be out before retrying it
-        keepLastServer: false
-      }
-    }
-  }
-*/
 
-const singleServerOptions = {
-    server: { 
-      server: 'localhost:5000', 
-      maxConnections: 5,
-      dangleSocketWaitTimeout: 5 * 60 * 1000, // time to wait for events on dangle socket
-    }
-    //ignoreNotStored: true, // ignore NOT_STORED response
-    //lifetime: 100, // TTL 100 seconds
-    //cmdTimeout: 3000, // command timeout in milliseconds
-    //connectTimeout: 8000, // connect to server timeout in ms
-    //compressor: require("custom-compressor"),
-    //logger: require("./custom-logger")
-  };
+const port              = 3000
+const client            = new MemcacheClient(multiServerOptions);
 
 app.get('/', async (req, res) => {
     res.send('Memcached Benchmark');
@@ -73,7 +60,6 @@ app.get('/', async (req, res) => {
 
 app.get('/stats', async (reg, res) => {
     try { 
-      const client = new MemcacheClient(multiServerOptions);
       const r = await client.cmd("stats");
       const html = 
         '<ul>' + 
@@ -88,13 +74,17 @@ app.get('/stats', async (reg, res) => {
 
 app.get('/store-and-fetch', async (req, res) => {
   try {
-    const client = new MemcacheClient(multiServerOptions);
     const cacheData = `data : ${Date.now()}`;
+    const key = `${Date.now()}`;
 
-    //const setResult = await client.set("key", cacheData);
-    const getResult = await client.get("key");
+    const setResult = await client.set(key, cacheData);
+    const getResult = await client.get(key);
 
-    res.send( getResult );
+    res.send( 
+      {
+        setResult: setResult, 
+        getResult: getResult
+      } );
 
   } catch( ex ) {
     console.error( ex );
